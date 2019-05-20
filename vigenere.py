a51b81ab759d2ef82e96d1f89f416dd7a35928d9
tracycaldwell <44450646+tracycaldwell@users.noreply.github.com>
2019-05-19 21:57:40 -0600

    Update vigenere.py

--- vigenere.py
+++ vigenere.py
@@ -1,7 +1,24 @@
 from helpers import alphabet_position, rotate_character
 
 def encrypt(text, key):
-    #Add your code here
-    return
+# take first letter of message if alpha
+# rotate by index of first letter of key
+# return new letter
+    encrypted_list = []
+    key_counter = 0
+    for i in text:
+        if i.isalpha() == True:
+            rot = alphabet_position(key[key_counter])
+            if key_counter < (len(key)-1):
+                key_counter += 1
+            else:
+                key_counter = 0
+        encrypted_list.append(rotate_character(i, rot))
+
+    return "".join(encrypted_list)
+def main():
+    print(encrypt(text = input("Type a message:"), key = input("Encryption key:")))
 
 
+if __name__ == "__main__":
+    main()
